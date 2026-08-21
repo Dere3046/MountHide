d037be6f8cd4a1d13f523b2e08de1342e3f6a61e
deref3046 <fine4trn@163.com>
2026-08-21 14:04:14 +0800

    add deps.mk

---
+++ deps.mk
@@ -0,0 +1,4 @@
+DEPS_LIB_NAME := MountHide
+DEPS_LIB_OBJS := LIB/mh_core.o LIB/mh_rule.o LIB/mh_reg.o LIB/mh_scan.o LIB/mh_umount.o LIB/mh_ver.o
+DEPS_LIB_INCS := LIB
+DEPS_LIB_DEPS := KallRecon HooKern Type_info
